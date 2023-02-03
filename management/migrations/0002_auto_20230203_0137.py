67150390569543e7b89a10b8b7d02f0cb8f9edce
jlhinojosa <jlhinojosa@gmail.com>
2023-02-02 23:12:48 -0300

    Update 0002_auto_20230203_0137.py

--- management/migrations/0002_auto_20230203_0137.py
+++ management/migrations/0002_auto_20230203_0137.py
@@ -13,13 +13,13 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='closingbalance',
             name='totalCarnes',
-            field=models.DecimalField(decimal_places=0, max_digits=10),
+            field=models.DecimalField(decimal_places=0, default=0, max_digits=10),
             preserve_default=False,
         ),
         migrations.AddField(
             model_name='closingbalance',
             name='totalGetnet',
-            field=models.DecimalField(decimal_places=0, max_digits=10),
+            field=models.DecimalField(decimal_places=0, default=0, max_digits=10),
             preserve_default=False,
         ),
     ]
