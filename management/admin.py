9f6af27998b5dbaaaf937d9dd97e57c067edff5b
jlhinojosa <jlhinojosa@gmail.com>
2023-02-05 20:38:04 -0300

    #16 Adding company

--- management/admin.py
+++ management/admin.py
@@ -5,12 +5,16 @@ from .models import *
 class DenominationAdmin(admin.ModelAdmin):
     list_display = ('id', 'value','type', 'displayOrder')
 
+class CompanyAdmin(admin.ModelAdmin):
+    list_display = ('name', 'address')
+
 class VenueAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name')
+    list_display = ('id', 'name', 'company')
 
 class DrawerAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'venue')
 
 admin.site.register(Denomination, DenominationAdmin)
+admin.site.register(Company, CompanyAdmin)
 admin.site.register(Venue, VenueAdmin)
 admin.site.register(Drawer, DrawerAdmin)
